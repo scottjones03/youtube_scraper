e18d99f77dc57b9a773c652e93cadfb1b8ac296f
scottjones03 <100515026+scottjones03@users.noreply.github.com>
2022-02-27 12:25:29 +0000

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# -----------------------------------------------------------------------------------------------------
-# Youtube Minecraft Scraper - All Rights Reserved
-# -----------------------------------------------------------------------------------------------------
-# Copyright (C) Scott Jones - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Authors:
-# - Scott Jones <scott.jones9336@gmail.com>
-# -----------------------------------------------------------------------------------------------------
