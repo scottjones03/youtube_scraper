101c22c036681d85471207c6b85822ac787c6e44
scottjones03 <100515026+scottjones03@users.noreply.github.com>
2022-05-05 19:45:33 +0100

    Update app.py

--- ytapi/pyside/app.py
+++ ytapi/pyside/app.py
@@ -32,13 +32,8 @@ def createApplication(
     styleSheet = qdarkstyle.load_stylesheet_pyside2()
     app.setStyleSheet(styleSheet)
 
-    # Following two lines are needed for setting the taskbar icon (https://stackoverflow.com/questions/14900510/changing-the-application-and-taskbar-icon-python-tkinter)
-    # The appid string determines the id of the app. So if this is the same as AMP, then they will be considered the same app by windows and styleOriginal only one icon will appear for both AMP and DMP if they are both open. Needs to be unique
     myappid = f"{nameApp.lower()}.{namePublisher.lower()}.gui"
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
-
-    # with open(f"style{PyQt.__name__}.txt", "w") as f:
-    #    f.write(styleSheet)
 
     print(f"Main application PID: {os.getpid()}")
 
