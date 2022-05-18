bd50f8feb090a60070d70f73f464b573c1e1e2e4
Scott Jones <scott.jones9336@gmail.com>
2022-05-18 07:47:45 +0100

    add copyright

--- ytapi/backend.py
+++ ytapi/backend.py
@@ -1,3 +1,12 @@
+# -----------------------------------------------------------------------------------------------------
+# Youtube Minecraft Scraper - All Rights Reserved
+# -----------------------------------------------------------------------------------------------------
+# Copyright (C) Scott Jones - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Authors:
+# - Scott Jones <scott.jones9336@gmail.com>
+# -----------------------------------------------------------------------------------------------------
 from typing import List, Optional
 
 from PySide6 import QtCore
